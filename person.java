727f2e8931cfb9b76bada06b2cc9c109c8edb19f
defanamaghfirotulula <80415681+defanamaghfirotulula@users.noreply.github.com>
2021-03-16 09:57:20 -0700

    Add files via upload

---
+++ person.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author asus
+ */
+public class person {
+    public static void main(String[]args){
+        String fName = "Defana M U";
+        String stuId = "E41200202";
+        String stuStatus = "Active";
+        
+        System.out.println("Nama    = " +fName);
+        System.out.println("NIM     = " +stuId);
+        System.out.println("Status  = " +stuStatus);
+    }
+}
